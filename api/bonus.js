159eae8d9632f59073690475ed75747f88a7e24a
phanuwat27422-lab <phanuwat27422@gmail.com>
2026-09-15 20:48:21 +0700

    Update bonus.js

--- api/bonus.js
+++ api/bonus.js
@@ -106,6 +106,7 @@ export default async function handler(req, res) {
     return res.status(200).json({
       updatedAt: new Date().toISOString(),
       currentGameweekLocked,
+      latestLockedGameweek,
       leaderboard,
     });
   } catch (err) {
